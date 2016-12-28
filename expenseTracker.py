b06550cf110de0c0cc775a4d3860947f5d96c333
Rahul Singh Dhek <rahulsinghdhek@gmail.com>
2016-12-28 11:49:17 +0530

    added the function to calculate total amount and net balance

--- expenseTracker.py
+++ expenseTracker.py
@@ -13,11 +13,12 @@ class Player:
 
 class Game:
 
-    
+    totalAmountPerGame = 0
     def __init__(self,gameId,playerList,totalAmount):
 	self.gameId=gameId
 	self.playerList= playerList
 	self.totalAmount = totalAmount
+	Game.totalAmountPerGame = Game.totalAmountPerGame + totalAmount
     def perHeadGameWise(self):
 	noOfPlayerForGame =  len(self.playerList)
 	return (self.totalAmount/noOfPlayerForGame)
@@ -56,10 +57,41 @@ print game1.isPartOfGame("Rivu")
 gameList=[game1,game2,game3,game4,game5]
 #print "Vikalp : " , Player.totalSpent['vikalp']/Player.totalGames['vikalp']
 #print "Rivu   : " , Player.totalSpent['Rivu']/Player.totalGames['Rivu']
-tot=0
+totalExp = defaultdict(int)
 for x in gameList:
     if (x.isPartOfGame("vikalp")):
-	tot = tot + x.perHeadGameWise()
+	totalExp['vikalp'] = totalExp['vikalp'] + x.perHeadGameWise()
+    if (x.isPartOfGame("Rivu")):
+	totalExp['Rivu'] = totalExp['Rivu'] + x.perHeadGameWise()
+    if (x.isPartOfGame("rahul")):
+	totalExp['rahul'] = totalExp['rahul'] + x.perHeadGameWise()
+    if (x.isPartOfGame("Uma")):
+	totalExp['Uma'] = totalExp['Uma'] + x.perHeadGameWise()
+    if (x.isPartOfGame("Deep")):
+	totalExp['Deep'] = totalExp['Deep'] + x.perHeadGameWise()
+    if (x.isPartOfGame("sudeep")):
+	totalExp['sudeep'] = totalExp['sudeep'] + x.perHeadGameWise()
+    if (x.isPartOfGame("Rejy")):
+	totalExp['Rejy'] = totalExp['Rejy'] + x.perHeadGameWise()
+    if (x.isPartOfGame("lynal")):
+	totalExp['lynal'] = totalExp['lynal'] + x.perHeadGameWise()
+    if (x.isPartOfGame("Rk")):
+	totalExp['Rk'] = totalExp['Rk'] + x.perHeadGameWise()
 
-print "vikalp balance : " , Player.totalDeposited['vikalp']-tot
+
+print "vikalp balance : " , Player.totalDeposited['vikalp']-totalExp['vikalp']
+print "rahul balance : " , Player.totalDeposited['rahul']-totalExp['rahul']
+print "Rivu balance : " , Player.totalDeposited['Rivu']-totalExp['Rivu']
+print "Uma balance : " , Player.totalDeposited['Uma']-totalExp['Uma']
+print "Deep balance : " , Player.totalDeposited['Deep']-totalExp['Deep']
+print "sudeep balance : " , Player.totalDeposited['sudeep']-totalExp['sudeep']
+print "lynal balance : " , Player.totalDeposited['lynal']-totalExp['lynal']
+print "Rejy balance : " , Player.totalDeposited['Rejy']-totalExp['Rejy']
+print "Rk balance : " , Player.totalDeposited['Rk']-totalExp['Rk']
+
+
+print "Net Amount on per game basis : " , Game.totalAmountPerGame
+print "Net Balance on game basis :    " , sum(Player.totalDeposited.values())- Game.totalAmountPerGame
+
+
     
